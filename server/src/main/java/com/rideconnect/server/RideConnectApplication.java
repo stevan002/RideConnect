52d7b8dd2ea9673e340a0b9499e0dc59e67424c2
stevan002 <stevanstankovic603@gmail.com>
2024-10-05 15:38:38 +0200

    Create project and connect with mysql database

---
+++ server/src/main/java/com/rideconnect/server/RideConnectApplication.java
@@ -0,0 +1,13 @@
+package com.rideconnect.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideConnectApplication.class, args);
+	}
+
+}
